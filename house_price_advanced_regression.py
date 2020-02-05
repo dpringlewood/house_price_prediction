d8382ee46e5e6f72cd2bf5640418819cd3a1ed66
dpringlewood <dpringlewood@gmail.com>
2020-02-05 12:07:14 +0400

    Master Branch

--- house_price_advanced_regression.py
+++ house_price_advanced_regression.py
@@ -11,15 +11,16 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 pd.set_option('display.max_columns', 100)
 
 house_data = pd.read_csv(r'./train.csv')
-print(house_data.head())
-print(house_data.info())
+# print(house_data.head())
+# print(house_data.info())
 
 # Do some visual-EDA
-sns.scatterplot('PoolArea', 'SalePrice', data=house_data[house_data['PoolArea'] != 0])
+# sns.scatterplot('PoolArea', 'SalePrice', data=house_data[house_data['PoolArea'] != 0])
 
 """
 Looking at the data initially we can see some fields have 
@@ -67,4 +68,57 @@ house_data.fillna(value=fill_values, inplace=True)
 # Replace the PoolQC with 1
 house_data[house_data['PoolQC'] != 0] = 1
 print(house_data.info())
+
+"""
+The last bit of data to impute is the LotFrontage parameter. My guess is that
+this can be estimated by the LotArea, but I want to explore the data visually
+first to support my hypothesis, and see if there is any outliers etc
+"""
+
+# sns.scatterplot(data=house_data, x='LotArea', y='LotFrontage')
+# plt.show()
+print(house_data['LotFrontage'].sort_values(ascending=False).head(25))
+
+"""
+We can see that there seems to be a linear relationship between the size of the lot
+and the Lot Frontage. However this is skewed by some outliers. Potentially we can
+look at getting rid of the outliers to make our imputation more accurate.
+"""
+
+print(house_data[house_data['LotFrontage'].isnull()]['LotArea'].describe())
+print(house_data[house_data['LotFrontage'].isnull()]['LotArea'].sort_values(ascending=False))
+
+lot_frontage_mask = (house_data['LotFrontage'].isnull()) & (house_data['LotArea'] < 40000)
+sns.swarmplot(data=house_data[lot_frontage_mask], y='LotArea', x='MSZoning')
+plt.show()
+
+sns.scatterplot(data=house_data[house_data['LotArea'] < 40000],
+                x='LotArea', y='LotFrontage')
+plt.show()
+
+
+"""
+I'm going to try something new here to test my skills and hopefully come up with a more
+accurate estimation of the LotFrontage variable. I will actually fit it to a simple linear
+regression against the LotArea, and use that to calculate the missing values. I will exclude
+outliers with a LotArea over 40,000. This is only 6 properties but skews the data.
+"""
+
+# Initialize the LinearRegression model
+
+linear = LinearRegression()
+
+# Fit the regression model on my available data & filter out outliers
+train_mask = (house_data['LotArea']<40000) & (house_data['LotFrontage'].notnull())
+X_train = np.array(house_data[train_mask].loc[:,'LotArea'])
+y_train = np.array(house_data[train_mask]['LotFrontage'])
+
+# Reshape my data to a 2D array for sklearn
+X_train = X_train.reshape(-1, 1)
+y_train = y_train.reshape(-1, 1)
+
+# Fit my model
+linear.fit(X_train, y_train)
+print('Intercept is: ', linear.intercept_)
+print('Coefficient is: ',linear.coef_)
 
