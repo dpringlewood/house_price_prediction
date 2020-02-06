950f8d05b06f44dfd7d2cb50e0038569d8d37bd4
dpringlewood <dpringlewood@gmail.com>
2020-02-06 11:13:47 +0400

    Master Branch

--- house_price_advanced_regression.py
+++ house_price_advanced_regression.py
@@ -11,7 +11,12 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error
+
 
 pd.set_option('display.max_columns', 100)
 
@@ -28,6 +33,8 @@ missing data. Some of these we can drop as they have many
 missing values, and intuitively they may not have a huge
 factor on the final price of a house. These are:
 Alley, MiscFeature.
+* I leave MiscFeature Value in there, as I assume that the
+value of the MiscFeature is the same to the buyer and seller.
 """
 
 house_data.drop(['Alley', 'MiscFeature', 'PoolArea'], inplace=True, axis=1)
@@ -89,7 +96,7 @@ print(house_data[house_data['LotFrontage'].isnull()]['LotArea'].describe())
 print(house_data[house_data['LotFrontage'].isnull()]['LotArea'].sort_values(ascending=False))
 
 lot_frontage_mask = (house_data['LotFrontage'].isnull()) & (house_data['LotArea'] < 40000)
-sns.swarmplot(data=house_data[lot_frontage_mask], y='LotArea', x='Neighborhood')
+sns.swarmplot(data=house_data[lot_frontage_mask], y='LotArea', x='MSZoning')
 plt.show()
 
 sns.scatterplot(data=house_data[house_data['LotArea'] < 40000],
@@ -132,6 +139,60 @@ use the .predict() method
 # Fill in the missing values
 for i in house_data[house_data['LotFrontage'].isnull()].index:
      house_data.loc[i, 'LotFrontage'] = (house_data.loc[i, 'LotArea']*linear.coef_[0][0])+linear.intercept_[0]
+
+"""
+Data wrangling is far from over. Even though I now have a complete dataset, the quality
+of the data has to be inspected, and cleaned up.
+"""
+
+# Take a quick look that all the qualitative data matches the description.
+for i in house_data.select_dtypes('object').columns:
+    print(i)
+    print(house_data[i].value_counts())
+
+"""
+It looks OK, there are a few 1 values, and I assume that this is where there is no
+data. Not really and issue as these will be converted into dummy variables for a 
+linear regression in the future.
+
+Lets look at the numerical values to see if there are any obvious correlations in 
+the data, or any massive ourliers. It will also be good to see how our sales price 
+is distributed.
+"""
+
+sns.distplot(house_data['SalePrice'])
+
+"""
+Says something about the price
+"""
+# Get the dummies variables and assign to the house_data df.
+house_data = pd.get_dummies(house_data, drop_first=True)
+
+#Get our target variable 'SalePrice'
+X = np.array(house_data.drop('SalePrice', axis=1))
+y = np.array(house_data['SalePrice'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3)
+y_train = y_train.reshape(-1, 1)
+linear = LinearRegression()
+lasso = Lasso(alpha=1, normalize=True, max_iter=100000)
+
+# steps = [('scaler', StandardScaler()),
+#           ('linear', LinearRegression())]
+# pipeline = Pipeline(steps)
+# linear_scaled = pipeline.fit(X_train, y_train)
+# print('You are only so good: ', linear_scaled.score(X_test, y_test))
+linear.fit(X_train, y_train)
+lasso.fit(X_train, y_train)
+print('Linear Score is: ',linear.score(X_test, y_test))
+print('Lasso Score is: ', lasso.score(X_test, y_test))
+
+y_pred_linear = linear.predict(X_test)
+y_pred_lasso = lasso.predict(X_test)
+
+print('MSE Linear: ', (mean_squared_error(y_test, y_pred_linear)**(1/2)))
+print('MSE Linear: ', (mean_squared_error(y_test, y_pred_lasso)**(1/2)))
+
 
 
 
